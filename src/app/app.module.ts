1dd93f1a7887f085f2dce34adab5f453f74796d4
Jamian Rajakone <jamian2011@gmail.com>
2020-06-16 11:52:50 +0200

    calculate jeton value and get it, add reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { JetonexchangeComponent } from './jetonexchange/jetonexchange.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { JetonexchangeComponent } from './jetonexchange/jetonexchange.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/jetonexchange/jetonexchange.component.ts
+++ src/app/jetonexchange/jetonexchange.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-jetonexchange',
@@ -8,16 +10,35 @@ import { Component, OnInit } from '@angular/core';
 export class JetonexchangeComponent implements OnInit {
   public currentJeton: number = 300
   public jetonfactor: number = 5
+  public form: FormGroup
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      jeton: ['', Validators.required],
+      amount: ['', Validators.required]
+    })
   }
 
   txtChanged(e): void {
     let jetons = e.target.value
     let amount = jetons * this.jetonfactor
 
+    this.setAmount(jetons, amount)
   }
 
+  private setAmount(jeton: number, value: number): void {
+    this.form.setValue({
+      jeton: jeton,
+      amount: value
+    })
+  }
+
+  submit(): void {
+    if (!this.form.valid) return
+    let jeton = this.form.get('jeton').value
+    //TODO send request
+    this.router.navigate(['/'])
+  }
 }
